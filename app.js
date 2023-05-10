5af136452f3028862259ae80e93a913b52ef6bf6
Peter Bulloch <pbulloch123@gmail.com>
2023-05-10 17:43:16 -0600

    added 404 catch all

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 connectDB();
 
 // Routes
-const statesRoutes = require('./routes/states');
+const states = require('./routes/states');
 const getRoutes = require('./routes/getRoutes');
 const patchRoutes = require('./routes/patchRoutes');
 const deleteRoutes = require('./routes/deleteRoutes');
@@ -24,5 +24,26 @@ app.use('/api/get', getRoutes);
 app.use('/api/patch', patchRoutes);
 app.use('/api/delete', deleteRoutes);
 app.use('/api/post', postRoutes);
+
+// Catch-all route handler for 404 errors
+app.use((req, res, next) => {
+    // Set status to 404
+    res.status(404);
+  
+    // Respond with HTML page if client accepts "text/html"
+    if (req.accepts('html')) {
+      res.sendFile(__dirname + '/public/404.html');
+      return;
+    }
+  
+    // Respond with JSON object if client accepts "application/json"
+    if (req.accepts('json')) {
+      res.json({ error: '404 Not Found' });
+      return;
+    }
+  
+    // Default to plain-text response
+    res.type('txt').send('404 Not Found');
+  });
 
 module.exports = app;
