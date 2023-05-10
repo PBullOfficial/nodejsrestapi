e21f624f0b35ca599118a8117533ede052fd4617
Peter Bulloch <pbulloch123@gmail.com>
2023-05-10 12:20:19 -0600

    added express to server.js

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 require('dotenv').config();
+const express = require('express');
 const { app } = require('./app');
 const connectDB = require('./db');
 
 const PORT = process.env.PORT || 3000;
 
-connectDB()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server started on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error(err);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
   });
+}).catch((err) => {
+  console.error(err);
+});
