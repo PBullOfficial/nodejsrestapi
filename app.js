20ab6ed7239a08f3b34ec3d105a0e7d58866e1de
Peter Bulloch <pbulloch123@gmail.com>
2023-05-10 15:09:21 -0600

    added postRoutes.js

--- app.js
+++ app.js
@@ -16,11 +16,13 @@ const statesRoutes = require('./routes/states');
 const getRoutes = require('./routes/getRoutes');
 const patchRoutes = require('./routes/patchRoutes');
 const deleteRoutes = require('./routes/deleteRoutes');
+const postRoutes = require('./routes/postRoutes');
 
 // Mount routes
 app.use('/api/states', statesRoutes);
 app.use('/api/get', getRoutes);
 app.use('/api/patch', patchRoutes);
 app.use('/api/delete', deleteRoutes);
+app.use('/api/post', postRoutes);
 
 module.exports = app;
