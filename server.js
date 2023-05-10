d45acefc0e7a25402516798f7d39486fc8af6f3f
Peter Bulloch <pbulloch123@gmail.com>
2023-05-10 12:49:19 -0600

    prettified server.js

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-require('dotenv').config();
-const express = require('express');
-const { app } = require('./app');
-const connectDB = require('./db');
+require("dotenv").config();
+const express = require("express");
+const { app } = require("./app");
+const connectDB = require("./db");
 
 const PORT = process.env.PORT || 3000;
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
   });
-}).catch((err) => {
-  console.error(err);
-});
