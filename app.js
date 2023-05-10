d9cc53874f76891496ff6cf3268bd0f180b7a336
Peter Bulloch <pbulloch123@gmail.com>
2023-05-10 15:05:23 -0600

    modularized routes with placeholder in delete and patch

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const statesRoutes = require('./routes/states');
 const connectDB = require('./db');
 
 const app = express();
+
+// Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Connect to database
 connectDB();
 
+// Routes
+const statesRoutes = require('./routes/states');
+const getRoutes = require('./routes/getRoutes');
+const patchRoutes = require('./routes/patchRoutes');
+const deleteRoutes = require('./routes/deleteRoutes');
+
+// Mount routes
 app.use('/api/states', statesRoutes);
+app.use('/api/get', getRoutes);
+app.use('/api/patch', patchRoutes);
+app.use('/api/delete', deleteRoutes);
 
 module.exports = app;
