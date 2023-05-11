36967dee08d274244751163c1434e6b5c31ddb22
Peter Bulloch <pbulloch123@gmail.com>
2023-05-10 18:03:17 -0600

    set up cors

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const connectDB = require('./db');
 
 const app = express();
 
+// Set up CORS
+const whitelist = ['https://dazzling-snickerdoodle-777101.netlify.app', 'https://lapis-skillful-walkover.glitch.me'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+app.use(cors(corsOptions));
+
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static('public'));
 
 // Connect to database
 connectDB();
@@ -26,25 +39,16 @@ app.use('/api/patch', patchRoutes);
 app.use('/api/delete', deleteRoutes);
 app.use('/api/post', postRoutes);
 
-// Catch-all route handler for 404 errors
+// Catch-all 404 handler
 app.use((req, res, next) => {
-    // Set status to 404
-    res.status(404);
-  
-    // Respond with HTML page if client accepts "text/html"
-    if (req.accepts('html')) {
-      res.sendFile(__dirname + '/public/404.html');
-      return;
-    }
-  
-    // Respond with JSON object if client accepts "application/json"
-    if (req.accepts('json')) {
-      res.json({ error: '404 Not Found' });
-      return;
-    }
-  
-    // Default to plain-text response
-    res.type('txt').send('404 Not Found');
-  });
+  res.status(404);
+  if (req.accepts('html')) {
+    res.sendFile(__dirname + '/public/404.html');
+  } else if (req.accepts('json')) {
+    res.json({ error: '404 Not found' });
+  } else {
+    res.type('txt').send('404 Not found');
+  }
+});
 
 module.exports = app;
