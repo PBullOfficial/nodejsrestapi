33deb85d6b7300d7d3f945c95b77a4fcf05db96e
Peter Bulloch <pbulloch123@gmail.com>
2023-08-07 13:00:21 -0600

    removed allowedOrigins.js and corsOptions.js as they were for personal testing (and are now handled by server.js)

--- config/allowedOrigins.js
+++
@@ -1,8 +0,0 @@
-const allowedOrigins = [
-    'http://localhost:3000',
-    'https://dazzling-snickerdoodle-777101.netlify.app',
-    'https://lapis-skillful-walkover.glitch.me'
-  ];
-  
-  module.exports = allowedOrigins;
-  
