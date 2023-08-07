ec71d238bfa0a8f8010171f8687209927f550f23
Peter Bulloch <pbulloch123@gmail.com>
2023-08-07 12:39:10 -0600

    updated readme, package.json. and server.js

---
+++ README.md
@@ -0,0 +1,15 @@
+**Description:**
+
+Final Project for INF653.  
+
+**To Run this code:**
+
+- Open a terminal window in the project folder. 
+  
+- Type: npm install
+  
+- All of the project dependencies will be installed.
+  
+- Add your own .env file with DATABASE_URI connection string value
+  
+- Type: npm run dev
--- server.js
+++ server.js
@@ -1,16 +1,43 @@
-require("dotenv").config();
-const express = require("express");
-const app = require("./app");
-const connectDB = require("./db");
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
+const logger = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
+const mongoose = require('mongoose');
+const connectDB = require('./config/dbConn');
+const PORT = process.env.PORT || 3500;
 
-const PORT = process.env.PORT || 3000;
+// Connect to MongoDB
+connectDB();
 
-connectDB()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server started on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+// custom middleware logger
+app.use(logger);
+
+// Cross Origin Resource Sharing
+// Open to all as Public API.. 
+// but please do understand how to apply 
+// CORS options with allowed origins
+app.use(cors());
+
+// built-in middleware for json 
+app.use(express.json());
+
+//serve static files
+app.use('/', express.static(path.join(__dirname, '/public')));
+
+// routes 
+app.use('/', require('./routes/root'));
+app.use('/states', require('./routes/api/states'));
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
+});
