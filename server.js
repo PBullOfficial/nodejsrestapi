18f5fa4a495f99e3e284744886c1e8a108e7fb4d
Peter Bulloch <pbulloch123@gmail.com>
2023-08-07 12:45:40 -0600

    added errorHandler to server.js

--- server.js
+++ server.js
@@ -41,3 +41,10 @@ app.all('*', (req, res) => {
         res.type('txt').send("404 Not Found");
     }
 });
+
+app.use(errorHandler);
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
